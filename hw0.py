3bbc6faf2525a9e0af9c2d11f16119ea5855e588
CastroLab <castro.jasonb@gmail.com>
2025-08-22 15:17:08 -0400

    Initial implementation of Python homework template

---
+++ hw0.py
@@ -0,0 +1,23 @@
+# Welcome to your first Python homework!
+# This is a comment - Python ignores everything after the '#' symbol
+# Comments help explain what your code does
+
+
+# ORIENTING YOURSELF TO COMPUTER CODE
+# Bullet point 2
+# TODO 1: Add your own comment below here (don't forget the # symbol!)
+
+
+# Here's some legit python code. Any wild guesses what it does? :-) 
+print("Hello!")
+
+
+# USING A .py STARTER FILE TO COMPLETE HOMEWORK
+# Bullet point 2
+# TODO 2: Write a print statement that begins: "Something to know about me is..."
+# then add something you want to share about yourself. (Remember that I will see this!)
+
+
+# Bullet point 3
+# TODO 3: Create a string variable named sb and assign it the value "spongebob"
+sb = "Spongebob"
